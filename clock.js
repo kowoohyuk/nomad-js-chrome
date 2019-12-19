e5e90dab2d7602dac2fb846c2d5d7affc9ddbee8
kowoohyuk <kowoohyuk91@gmail.com>
2019-12-19 21:31:15 +0900

    step 2

--- clock.js
+++ clock.js
@@ -6,11 +6,12 @@ function getTime() {
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  clockTitle.innerHTML = `${hours}:${minutes < 10 ? 0+minutes : minutes}:${seconds < 10 ? 0+seconds : seconds}`;
+  clockTitle.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
   getTime();
+  setInterval(getTime, 1000);
 }
 
 init();
