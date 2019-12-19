598c4199f3b9d536dd172a930f7e12a4f401b446
kowoohyuk <kowoohyuk91@gmail.com>
2019-12-19 23:59:56 +0900

    step 10

--- weather.js
+++ weather.js
@@ -1,4 +1,19 @@
+const weather = document.querySelector('.js-weather');
+
+const API_KEY = 'a97555fa13d5404c0b8078c4aee320b3';
 const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  ).then( response =>
+    response.json()
+  ).then( json => {
+    const temperature = json.main.temp;
+    const place = json.name;
+    weather.innerText = `${temperature} @ ${place}`;
+  } );
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +27,7 @@ function handleGeoSucces(position) {
     longitude
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -27,7 +43,8 @@ function loadCoords() {
   if(loadedCoords === null) {
     askForCoords();
   } else {
-      //getWeather
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
   }
 }
 
